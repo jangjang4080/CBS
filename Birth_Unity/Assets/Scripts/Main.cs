c7249099f61634c5f0c6d822950f328f602293a3
Jang <coolcoolii@naver.com>
2017-09-16 18:07:29 +0900

    Birth_Unity

---
+++ Birth_Unity/Assets/Scripts/Main.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TWICE
+{
+	public class Main : MonoBehaviour 
+	{
+		void Start () 
+		{
+			Quiz quiz = GameObject.FindObjectOfType<Quiz>();
+			if (quiz != null)
+			{
+				quiz.Init();
+			}
+		}
+		
+		void Update () 
+		{
+			
+		}
+	}
+}
---
+++ Birth_Unity/Assets/Scripts/Quiz.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TWICE
+{
+	public class Quiz : MonoBehaviour 
+	{
+		public Camera mainCamera;
+		public GameObject startButton;
+
+		public void Init()
+		{
+
+		}
+
+		public void OnStart()
+		{
+			GlobalAction.ButtonPushScale(startButton, 0.2f);
+
+			Invoke("GoToQuiz", 0.2f);
+		}
+
+		void GoToQuiz()
+		{
+			mainCamera.transform.localPosition = new Vector3(3000f, 0f, 0f);
+		}
+	}	
+}
